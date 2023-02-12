16ece9be41bd6507461ddd37061457e318b91f28
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-02-11 22:22:27 -0500

    Initial Commit

---
+++ SavePercentage/SavePercentageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavePercentageApp.swift
+//  SavePercentage
+//
+//  Created by Lewis Brocklehurst on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct SavePercentageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
