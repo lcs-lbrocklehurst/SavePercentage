536e925abd748746fa23e0e74a1261002fbfa28f
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-19 12:19:35 -0400

    Added a model view and a struct

---
+++ SavePercentage/Model/SavePercentage.swift
@@ -0,0 +1,16 @@
+//
+//  SavePct.swift
+//  SavePercentage
+//
+//  Created by Lewis Brocklehurst on 2023-04-19.
+//
+
+import Foundation
+
+struct SavePercentage: Identifiable {
+    var id: Int
+    let saves: Int
+    let shots: Int
+    let savePercentage: Double
+    let title: String
+}
