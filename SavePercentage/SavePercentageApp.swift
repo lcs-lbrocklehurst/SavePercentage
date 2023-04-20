f2a6e35e6c24aad154060bc401d89c3318b06772
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-20 11:21:57 -0400

    Added helper code that copies the database into the app's sandbox and establishes a connection to the database.

--- SavePercentage/SavePercentageApp.swift
+++ SavePercentage/SavePercentageApp.swift
@@ -12,6 +12,8 @@ struct SavePercentageApp: App {
     var body: some Scene {
         WindowGroup {
             SavePercentageView()
+            //Make the database available to all child views through the environment
+                .environment(\.blackbirdDatabase, AppDatabase.instance)
         }
     }
 }
