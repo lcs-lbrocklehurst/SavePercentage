7fe05ebbdb55b3a734c774613cd09bdf0c0038fa
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-20 11:25:22 -0400

    Adjusted the model so that a joke can be read from or written to the database.

--- SavePercentage/Model/SavePercentage.swift
+++ SavePercentage/Model/SavePercentage.swift
@@ -4,15 +4,15 @@
 //
 //  Created by Lewis Brocklehurst on 2023-04-19.
 //
-
+import Blackbird
 import Foundation
 
-struct SavePercentage: Identifiable {
-    var id: Int
-    let saves: Int
-    let shots: Int
-    let savePercentage: Double
-    let title: String
+struct SavePercentage: Identifiable, BlackbirdModel {
+    @BlackbirdColumn var id: Int
+    @BlackbirdColumn var saves: Int
+    @BlackbirdColumn var shots: Int
+    @BlackbirdColumn var savePercentage: Double
+    @BlackbirdColumn var title: String
 }
 
 let exampleSavePercentage = SavePercentage(id: 1 , saves: 9, shots: 10, savePercentage: 0.900, title: "Game 1")
